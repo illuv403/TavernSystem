a11637f6b50ab8fb7782b8bfc8a2c49fb650ddd5
illuv403 <s31388@pjwstk.edu.pl>
2025-05-09 10:32:51 +0200

    Created Models project and added models

---
+++ TavernSystem.Application/Adventurer.cs
@@ -0,0 +1,10 @@
+namespace TavernSystem.Application;
+
+public class Adventurer
+{
+    public int Id { get; set; }
+    public string Nickname { get; set; }
+    public int RaceId { get; set; }
+    public int ExperienceId { get; set; }
+    public string PersonId { get; set; }
+}
---
+++ TavernSystem.Application/Race.cs
@@ -0,0 +1,7 @@
+namespace TavernSystem.Application;
+
+public class Race
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
